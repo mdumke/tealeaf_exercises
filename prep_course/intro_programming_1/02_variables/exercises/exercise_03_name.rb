c975d367b1aa395948c6b6a4b7de96c40425d605
Matthias Dumke <matthias.dumke@gmx.net>
2015-07-20 12:54:50 +0200

    exercises for chapter 2: variables

---
+++ prep_course/intro_programming_1/02_variables/exercises/exercise_03_name.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+# exercise 3: print something 10 times
+print "If there is anything you'd like to tell the world 10 times, here's you chance: "
+
+message = gets.chomp
+
+10.times do
+  puts message
+end
---
+++ prep_course/intro_programming_1/02_variables/exercises/exercise_04_name.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+# exercise 4: concatenating first name and last name
+puts "Please fill out our form"
+
+print "first name: "
+first_name = gets.chomp
+
+print "last name: "
+last_name = gets.chomp
+
+puts "\nThank you, #{first_name} #{last_name}!"
