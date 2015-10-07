aa8eda5be08d49e4a5e5c9b29a9226909f9d0489
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-07 22:54:57 +0200

    triplet exercise v2

--- live_sessions/2015_10_10_triplet.rb
+++ live_sessions/2015_10_10_triplet.rb
@@ -1,12 +1,23 @@
 class Triplet
   def self.where(options = {})
-    fail(ArgumentError, ':max_factor missing') unless options[:max_factor]
+    triplets = []
 
-    Triplet.new.where(options)
+    possible_values(options) do |a, b, c|
+      triplet = Triplet.new(a, b, c)
+
+      next unless triplet.pythagorean?
+      next unless triplet.sum == options[:sum] if options[:sum]
+
+      triplets << triplet
+    end
+
+    triplets
   end
 
-  def initialize(a = nil, b = nil, c = nil)
-    @a, @b, @c = [a, b, c]
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
   end
 
   def sum
@@ -21,42 +32,16 @@ class Triplet
     @a**2 + @b**2 == @c**2
   end
 
-  def where(options = {})
+  def self.possible_values(options)
     min = options[:min_factor] || 1
     max = options[:max_factor]
-  
-    triplets = []
-  
-    permutations(min, max) do |a, b, c|
-      t = Triplet.new(a, b, c)
-      triplets << t if t.pythagorean?
-    end
 
-    triplets.select! { |t| t.sum == options[:sum] } if options[:sum]
-    triplets
-  end
-
-  private
-
-  def permutations(min, max)
-    a = min
-    
-    while a < max
-      b = a
-
-      while b < max
-        c = b
-
-        while c <= max
+    min.upto(max) do |a|
+      a.upto(max) do |b|
+        b.upto(max) do |c|
           yield a, b, c
-
-          c += 1
         end
-
-        b += 1
       end
-    
-      a += 1
     end
   end
 end
