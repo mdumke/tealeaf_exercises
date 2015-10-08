6f2c02f08bf641e8afce1f6486ffaef13001cf09
Matthias Dumke <matthias.dumke@gmx.net>
2015-10-08 20:35:18 +0200

    refactoring the triplet exercise

--- live_sessions/2015_10_10_triplet.rb
+++ live_sessions/2015_10_10_triplet.rb
@@ -1,33 +1,15 @@
 class Triplet
   def self.where(options)
-    result = []
-
-    possible_triplets(options) do |triplet|
-      next unless triplet.valid?(options)
-
-      result << triplet
-    end
-
-    result
+    possible_triplets(options).select(&:valid?)
   end
 
   def self.possible_triplets(options)
-    min = options[:min_factor] || 1
-    max = options[:max_factor]
-
-    min.upto(max - 1) do |a|
-      a.upto(max - 1) do |b|
-        b.upto(max) do |c|
-          yield Triplet.new(a, b, c)
-        end
-      end
-    end
+    TripletsEnumerator.new(options)
   end
 
-  def initialize(a, b, c)
-    @a = a
-    @b = b
-    @c = c
+  def initialize(a, b, c, required_sum = nil)
+    @a, @b, @c = a, b, c
+    @required_sum = required_sum
   end
 
   def sum
@@ -42,7 +24,27 @@ class Triplet
     @a**2 + @b**2 == @c**2
   end
 
-  def valid?(options)
-    pythagorean? && (!options[:sum] || sum == options[:sum])
+  def valid?
+    pythagorean? && (!@required_sum || sum == @required_sum)
+  end
+end
+
+class TripletsEnumerator
+  include Enumerable
+
+  def initialize(config)
+    @max = config[:max_factor] || fail(ArgumentError, 'max_factor required')
+    @min = config[:min_factor] || 1
+    @sum = config[:sum]
+  end
+
+  def each
+    @min.upto(@max - 1) do |a|
+      a.upto(@max - 1) do |b|
+        b.upto(@max) do |c|
+          yield Triplet.new(a, b, c, @sum)
+        end
+      end
+    end
   end
 end
